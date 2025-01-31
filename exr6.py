fded53e0c2633dc89a2f691679aa69199373e099
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-01-31 15:01:17 -0500

    Update exr6.py

--- exr6.py
+++ exr6.py
@@ -1,4 +1,4 @@
 nombre = input("Entrez un nombre : ")
-resultat = nombre * 2
+resultat = int(nombre) * 2
 print("Le double de votre nombre est :", resultat)
 #il faut dire a l'ordi que"nombre" est un nombre et non une chaine de caractère avec int(nombre)
