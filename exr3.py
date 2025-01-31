001dfe2bb5b10c4616134bba80df205c699e14e2
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-01-31 14:32:14 -0500

    Update exr3.py

--- exr3.py
+++ exr3.py
@@ -1,4 +1,4 @@
-def fonction (a,b):
-    c = a+b   #doit avoir un espace de chaque côté du =
+def a_and_b(a, b):#mettre un nom plus explicite pour la fonction
+    c = a + b   #doit avoir un espace de chaque côté du = et ,
     return c  #indentation manquante pour marquer le bloc
-print(fonction(3,4))
+print(a_and_b(3, 4))
