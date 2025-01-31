a01b4b361264bc2a64257d77df4a87973874b92a
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-01-31 14:25:15 -0500

    Create exr5.py

---
+++ exr5.py
@@ -0,0 +1,4 @@
+"""a-t-on fini?, si oui, fin du programme
+si non, regard si température >20 degrée celcius
+si temp>20 degrée celcius, allumer le chauffage 10 secondes et retour au début
+si temp<20 retour au début"""
