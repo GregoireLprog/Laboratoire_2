7bd64892604a6ee6825ae35e8a9f34bb8966dc9d
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-01-31 15:27:12 -0500

    Create exr8.py

---
+++ exr8.py
@@ -0,0 +1,7 @@
+def addition(a, b):
+    resultat = a + b
+    return resultat
+x = int(5)#indiquer avec int que 5 et 3 son un chiffre et non une chaine de caractère
+y = int(3)
+z = addition(x, y)
+print("Résultat:", z)
