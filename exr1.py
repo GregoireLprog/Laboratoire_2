f0aa9adc4cccb1072d00346dc5e7ef927b791fca
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-01-31 13:47:02 -0500

    Update exr1.py

--- exr1.py
+++ exr1.py
@@ -3,7 +3,7 @@ y = 3.14
 z = "Bonjour"
 a = True
 
-print (type(x))
+print (type(x)) # type() permet de connaitre le type de la variable et print pour l'afficher dans le terminal
 print (type(y))
 print (type(z))
 print (type(a))
